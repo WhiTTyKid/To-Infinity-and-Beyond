2939c205cc86b84a419ef89f7ea7266c6c7fd998
WhiTTyKid <34419332+WhiTTyKid@users.noreply.github.com>
2017-12-10 21:52:38 +0530

    Add files via upload

---
+++ Programme/src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+public static void main() {
+    int x;
+	while(x<10)
+	{
+		System.out.print(" "+x);
+		switch(x) 
+		{
+		case5:x+=2;continue;
+		
+		}
+	}
+}
+}
---
+++ Programme/src/prg1.java
@@ -0,0 +1,6 @@
+public class prg1{ 
+public static void main(String[] args) { 
+System.out.println("this is an infinite loop");
+for(int n=1;n>=0;n++)
+System.out.println(n);
+}}
